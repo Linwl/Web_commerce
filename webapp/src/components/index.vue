5c3a65331f8180413d48c0910cd9eb9a4e7ad9f9
Linwl <304115325@qq.com>
2017-10-29 22:23:49 +0800

    修改首页

--- webapp/src/components/index.vue
+++ webapp/src/components/index.vue
@@ -1,21 +1,35 @@
 <template>
-  <div>
-   <el-button type="primary" plain>主要按钮</el-button>
-   
-  </div>
+<div>
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#0B7BB3" text-color="#fff" active-text-color="#ffd04b">
+      <el-menu-item index="1"><router-link to ='/'>首頁</router-link></el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+      </el-submenu>
+      </el-menu-item>
+  </el-menu>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "index",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      activeIndex: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
